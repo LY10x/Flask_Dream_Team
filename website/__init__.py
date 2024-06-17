0be312bfdefef4cd7a6f3d0b1139a64d6879eb90
LY10x <LeoYoshi10@gmail.com>
2024-06-17 11:58:33 +0300

    initialized the database and made a sign_up that gets information

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'skibidi smeg'
+    app.config['SQLALCHEMY'] = f'sqlit:///{DB_NAME}'
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template  # type: ignore
+from flask import Blueprint, render_template, request, flash  # type: ignore
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,22 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fname = request.form.get('fname')
+        pass1 = request.form.get('pass1')
+        pass2 = request.form.get('pass2')
+
+        if len(email) < 4:
+            flash("email must be longer than 3 letters", category='error')
+        elif len(fname) < 2:
+            flash("Firstname must be longer than 2 letters", category='error')
+        elif pass1 != pass2:
+            flash("passwords dont match", category='error')
+        elif len(pass1) < 7:
+             flash("passwords must be longer than 7 char", category='error')
+        else:
+            flash('Account created!', category='success')
     return render_template("sign_up.html")
